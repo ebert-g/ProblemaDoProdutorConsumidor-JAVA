a7c4b682e0ba4d5fa6b71b4f323e1396a4cf4e17
Ebert Guerreiro <ebertg.est@gmail.com>
2025-04-30 17:28:50 -0300

    a

--- src/entidades/Consumidor.java
+++ src/entidades/Consumidor.java
@@ -3,7 +3,6 @@ package entidades;
 public class Consumidor implements Runnable {
     private final Buffer buffer;
 
-    // PEGA O BUFFER
     public Consumidor(Buffer buffer) {
         this.buffer = buffer;
     }
@@ -11,15 +10,11 @@ public class Consumidor implements Runnable {
     @Override
     public void run() {
         while (true) {
-            // try - TENTA RETIRAR ALGO DO BUFFER
             try {
-                // SEMPRE VAI ESPERAR PRA DE 0 1000 MILISEGUNDOS
                 int tempoEspera = (int) (Math.random() * 1000);
                 Thread.sleep(tempoEspera);
-                // CONSOME
                 buffer.consumir();
             } catch (InterruptedException e) {
-                // SE VAZIO AVISA E NÃO TENTA RETIRAR NADA
                 System.out.println("[Consumidor] Interrompido! ");
                 break;
 
--- src/entidades/Produtor.java
+++ src/entidades/Produtor.java
@@ -1,9 +1,7 @@
 package entidades;
-//IMPLEMENTA UMA INTERFACE QUE PERMITE USAR ESSA CLASSE COMO UM THREAD
 public class Produtor implements Runnable {
     private final Buffer buffer;
 
-    // PEGA O BUFFER
     public Produtor(Buffer buffer) {
         this.buffer = buffer;
     }
@@ -11,15 +9,11 @@ public class Produtor implements Runnable {
     @Override
     public void run() {
         while (true) {
-            // try - TENTA COLOCAR ALGO NO BUFFER
             try {
-                // SEMPRE VAI ESPERAR PRA DE 0 1000 MILISEGUNDOS
                 int tempoEspera = (int) (Math.random() * 1000);
                 Thread.sleep(tempoEspera);
-                // PRODUZ
                 buffer.produzir();
             } catch (InterruptedException e) {
-                // SE CHEIO AVISA E NÃO TENTA COLOCAR NADA
                 System.out.println("[Produtor] Interrompido! ");
                 break;
 
